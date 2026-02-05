427a818e4c85d1294216deb1fb82e8bf47818c82
Michal Ziemianek <mziemianek3@gmail.com>
2026-02-05 22:26:51 +0100

    feat(neovim): add smooth scrolling

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -264,11 +264,16 @@ require("lazy").setup({
 	{
 		"sphamba/smear-cursor.nvim",
 		opts = {
-			stiffness = 0.8,
-			trailing_stiffness = 0.5,
+			enabled = true,
+			smear_between_buffers = false,
+			stiffness = 0.6,
+			trailing_stiffness = 0.6,
 			distance_stop_animating = 0.5,
-			time_interval = 7, -- milliseconds
 		},
+	},
+	{
+		"karb94/neoscroll.nvim",
+		opts = {},
 	},
 })
 
