6abec00c0ff1dac2dc777cf43ebcc61b27e8115e
Michal Ziemianek <mziemianek3@gmail.com>
2026-02-05 21:46:11 +0100

    feat(neovim): add cursor smear plugin

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -261,6 +261,15 @@ require("lazy").setup({
 			require("treesitter-context").setup({})
 		end,
 	},
+	{
+		"sphamba/smear-cursor.nvim",
+		opts = {
+			stiffness = 0.8,
+			trailing_stiffness = 0.5,
+			distance_stop_animating = 0.5,
+			time_interval = 7, -- milliseconds
+		},
+	},
 })
 
 -- Handle persistent undo
