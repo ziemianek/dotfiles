72bcf744d4861b345e660966b8ab41f9dedc0a02
Michal Ziemianek <mziemianek3@gmail.com>
2026-02-18 07:29:57 +0100

    feat(neovim): more cursor smear

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -279,10 +279,10 @@ require("lazy").setup({
 		"sphamba/smear-cursor.nvim",
 		opts = {
 			enabled = true,
-			smear_between_buffers = false,
-			stiffness = 0.6,
-			trailing_stiffness = 0.6,
-			distance_stop_animating = 0.5,
+			-- smear_between_buffers = false,
+			-- stiffness = 0.6,
+			-- trailing_stiffness = 0.6,
+			-- distance_stop_animating = 0.5,
 		},
 	},
 	{
